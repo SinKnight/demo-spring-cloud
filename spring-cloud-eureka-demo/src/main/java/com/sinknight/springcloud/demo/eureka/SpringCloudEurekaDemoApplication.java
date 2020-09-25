d750b36ef757e1aefcc2fffa94b174059bd3f217
e <jiang.hd@belle.com.cn>
2020-09-25 14:10:07 +0800

    新增spring-cloud-eureka-demo项目

---
+++ spring-cloud-eureka-demo/src/main/java/com/sinknight/springcloud/demo/eureka/SpringCloudEurekaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sinknight.springcloud.demo.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudEurekaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudEurekaDemoApplication.class, args);
+	}
+
+}
