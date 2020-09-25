3320fc8546d6b7d66d125a4d80212b0a05d817f5
e <jiang.hd@belle.com.cn>
2020-09-25 15:04:11 +0800

    新增eureka服务配置

--- spring-cloud-eureka-demo/src/main/java/com/sinknight/springcloud/demo/eureka/SpringCloudEurekaDemoApplication.java
+++ spring-cloud-eureka-demo/src/main/java/com/sinknight/springcloud/demo/eureka/SpringCloudEurekaDemoApplication.java
@@ -2,8 +2,15 @@ package com.sinknight.springcloud.demo.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * 启动类
+ *
+ * @author k+
+ */
 @SpringBootApplication
+@EnableEurekaServer
 public class SpringCloudEurekaDemoApplication {
 
 	public static void main(String[] args) {
