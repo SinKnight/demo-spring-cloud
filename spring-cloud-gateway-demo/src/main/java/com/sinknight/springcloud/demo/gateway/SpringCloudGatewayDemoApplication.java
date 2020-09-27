816d97437bf274d9fb186a2cb5fa099f305454d3
e <jiang.hd@belle.com.cn>
2020-09-27 18:33:51 +0800

    修改配置文件编码

--- spring-cloud-gateway-demo/src/main/java/com/sinknight/springcloud/demo/gateway/SpringCloudGatewayDemoApplication.java
+++ spring-cloud-gateway-demo/src/main/java/com/sinknight/springcloud/demo/gateway/SpringCloudGatewayDemoApplication.java
@@ -3,6 +3,11 @@ package com.sinknight.springcloud.demo.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动类
+ *
+ * @author k+
+ */
 @SpringBootApplication
 public class SpringCloudGatewayDemoApplication {
 
