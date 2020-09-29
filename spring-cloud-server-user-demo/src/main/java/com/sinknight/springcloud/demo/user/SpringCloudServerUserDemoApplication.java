8bc8436ed9ecec18b76dc010b10de8be434e345a
e <jiang.hd@belle.com.cn>
2020-09-29 18:13:36 +0800

    新增项目用户服务demo

---
+++ spring-cloud-server-user-demo/src/main/java/com/sinknight/springcloud/demo/user/SpringCloudServerUserDemoApplication.java
@@ -0,0 +1,18 @@
+package com.sinknight.springcloud.demo.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author k+
+ */
+@SpringBootApplication
+public class SpringCloudServerUserDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudServerUserDemoApplication.class, args);
+	}
+
+}
---
+++ spring-cloud-server-user-demo/src/test/java/com/sinknight/springcloud/demo/user/SpringCloudServerUserDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinknight.springcloud.demo.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudServerUserDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
